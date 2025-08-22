f38cb1afb9c67b681f5f49cb25a84f18e77143f9
Daniel Harvey <keithdanielharvey@gmail.com>
2025-08-22 02:39:14 -0600

    local function

--- luarules/gadgets/team_transfer/game_enemy_transfer.lua
+++ luarules/gadgets/team_transfer/game_enemy_transfer.lua
@@ -1,18 +1,26 @@
 local TeamTransfer = VFS.Include("luarules/gadgets/team_transfer/api_gadgets.lua")
 
+local function shouldAllowResourceTransfer(ctx)
+	return ctx.isCheatingEnabled or ctx.senderIsNonPlayer or ctx.receiverIsNonPlayer
+end
+
+local function shouldAllowUnitTransfer(ctx)
+	if ctx.capture then
+		return true
+	end
+	return ctx.isCheatingEnabled or ctx.fromIsNonPlayer or ctx.toIsNonPlayer
+end
+
 TeamTransfer.RegisterPolicy(function(policy)
 	policy.ResourceTransfers.Enemy:Use(function(ctx)
-		if ctx.isCheatingEnabled or ctx.senderIsNonPlayer or ctx.receiverIsNonPlayer then
+		if shouldAllowResourceTransfer(ctx) then
 			return true
 		end
 		return false
 	end)
 
 	policy.UnitTransfers.Enemy:Use(function(ctx)
-		if ctx.capture then
-			return true
-		end
-		if ctx.isCheatingEnabled or ctx.fromIsNonPlayer or ctx.toIsNonPlayer then
+		if shouldAllowUnitTransfer(ctx) then
 			return true
 		end
 		return false
