beyond-all-reason/Beyond-All-Reason

Sharing 3/4: Tech Core / Keystone

units/ArmBuildings/TechCore/armkeystone.lua
@@ -1,0 +1,132 @@
+return {
+	armkeystone = {
+		activatewhenbuilt = true,
+		buildangle = 2048,
+		builddistance = 150,
+		builder = true,
+		buildoptions = {
+			[1] = "armvoussoir",
+		},
+		buildpic = "ARMGATET3.DDS",
+		buildtime = 15000,
+		workertime = 200,
+		onoffable = true,
+		canattack = false,
+		canrepeat = false,
+		category = "NOWEAPON",
+		collisionvolumeoffsets = "0 0 3",
+		collisionvolumescales = "91 85 91",
+		collisionvolumetype = "CylY",
+		energycost = 10000,
+		energyupkeep = -100,
+		energystorage = 1000,
+		explodeas = "fusionExplosion",
+		footprintx = 6,
+		footprintz = 6,
+		health = 2000,
+		idleautoheal = 5,
+		idletime = 1800,
+		maxacc = 0,
+		maxdec = 0,
+		maxslope = 10,
+		maxwaterdepth = 1000,
+		metalcost = 1000,
+		noautofire = true,
+		objectname = "Units/ARMGATET3.s3o",
+		script = "Units/armkeystone.lua",
+		seismicsignature = 0,
+		selfdestructas = "fusionExplosionSelfd",
+		sightdistance = 350,
+		customparams = {
+			buildinggrounddecaldecayspeed = 30,
+			buildinggrounddecalsizex = 9,
+			buildinggrounddecalsizey = 9,
+			buildinggrounddecaltype = "decals/armgate_aoplane.dds",
+			model_author = "Beherith/Protar",
+			normaltex = "unittextures/Arm_normal.dds",
+			removestop = true,
+			removewait = true,
+			shield_color_mult = 25,
+			shield_power = 200,
+			shield_radius = 150,
+			subfolder = "ArmBuildings/TechCore",
+			tech_core_value = 1,
+			techlevel = 1,
+			unitgroup = "energy",
+			usebuildinggrounddecal = true,
+		},
+		featuredefs = {
+			dead = {
+				blocking = true,
+				category = "corpses",
+				collisionvolumeoffsets = "0 0 3",
+				collisionvolumescales = "91 67 91",
+				collisionvolumetype = "CylY",
+				damage = 8500,
+				footprintx = 6,
+				footprintz = 6,
+				height = 20,
+				metal = 100,
+				object = "Units/armgatet3_dead.s3o",
+				resurrectable = 0,
+			},
+			heap = {
+				blocking = false,
+				category = "heaps",
+				damage = 4250,
+				footprintx = 5,
+				footprintz = 5,
+				height = 4,
+				metal = 150,
+				resurrectable = 0,
+			},
+		},
+		sfxtypes = {
+			pieceexplosiongenerators = {
+				[1] = "deathceg2",
+				[2] = "deathceg3",
+				[3] = "deathceg4",
+			},
+		},
+		sounds = {
+			canceldestruct = "cancel2",
+			underattack = "warning1",
+			cant = { [1] = "cantdo4" },
+			count = { [1] = "count6", [2] = "count5", [3] = "count4", [4] = "count3", [5] = "count2", [6] = "count1" },
+			ok = { [1] = "drone1" },
+			select = { [1] = "drone1" },
+		},
+		weapondefs = {
+			repulsor = {
+				avoidfeature = false,
+				craterareaofeffect = 0,
+				craterboost = 0,
+				cratermult = 0,
+				edgeeffectiveness = 0.15,
+				name = "PlasmaRepulsor",
+				range = 1,
+				weapontype = "Shield",
+				damage = { default = 1 },
+				shield = {
+					alpha = 0.17,
+					armortype = "shields",
+					exterior = true,
+					force = 2.5,
+					intercepttype = 951,
+					power = 200,
+					powerregen = 10,
+					powerregenenergy = 200,
+					radius = 100,
+					startingpower = 0,
+					visiblerepulse = true,
+				},
+			},
+		},
+		weapons = {
+			[1] = {
+				def = "REPULSOR",
+				onlytargetcategory = "NOTSUB",
+			},
+		},
+	},
+}

units/CorBuildings/TechCore/corkeystone.lua
@@ -1,0 +1,132 @@
+return {
+	corkeystone = {
+		activatewhenbuilt = true,
+		buildangle = 2048,
+		builddistance = 150,
+		builder = true,
+		buildoptions = {
+			[1] = "corvoussoir",
+		},
+		buildpic = "CORGATET3.DDS",
+		buildtime = 15000,
+		workertime = 200,
+		onoffable = true,
+		canattack = false,
+		canrepeat = false,
+		category = "NOWEAPON",
+		collisionvolumeoffsets = "0 0 3",
+		collisionvolumescales = "91 85 91",
+		collisionvolumetype = "CylY",
+		energycost = 10000,
+		energyupkeep = -100,
+		energystorage = 1000,
+		explodeas = "fusionExplosion",
+		footprintx = 6,
+		footprintz = 6,
+		health = 2000,
+		idleautoheal = 5,
+		idletime = 1800,
+		maxacc = 0,
+		maxdec = 0,
+		maxslope = 10,
+		maxwaterdepth = 1000,
+		metalcost = 1000,
+		noautofire = true,
+		objectname = "Units/CORGATET3.s3o",
+		script = "Units/corkeystone.lua",
+		seismicsignature = 0,
+		selfdestructas = "fusionExplosionSelfd",
+		sightdistance = 350,
+		customparams = {
+			buildinggrounddecaldecayspeed = 30,
+			buildinggrounddecalsizex = 9,
+			buildinggrounddecalsizey = 9,
+			buildinggrounddecaltype = "decals/corgate_aoplane.dds",
+			model_author = "Beherith/Protar",
+			normaltex = "unittextures/Cor_normal.dds",
+			removestop = true,
+			removewait = true,
+			shield_color_mult = 25,
+			shield_power = 200,
+			shield_radius = 150,
+			subfolder = "CorBuildings/TechCore",
+			tech_core_value = 1,
+			techlevel = 1,
+			unitgroup = "energy",
+			usebuildinggrounddecal = true,
+		},
+		featuredefs = {
+			dead = {
+				blocking = true,
+				category = "corpses",
+				collisionvolumeoffsets = "0 0 3",
+				collisionvolumescales = "91 67 91",
+				collisionvolumetype = "CylY",
+				damage = 8500,
+				footprintx = 6,
+				footprintz = 6,
+				height = 20,
+				metal = 100,
+				object = "Units/corgatet3_dead.s3o",
+				resurrectable = 0,
+			},
+			heap = {
+				blocking = false,
+				category = "heaps",
+				damage = 4250,
+				footprintx = 5,
+				footprintz = 5,
+				height = 4,
+				metal = 150,
+				resurrectable = 0,
+			},
+		},
+		sfxtypes = {
+			pieceexplosiongenerators = {
+				[1] = "deathceg2",
+				[2] = "deathceg3",
+				[3] = "deathceg4",
+			},
+		},
+		sounds = {
+			canceldestruct = "cancel2",
+			underattack = "warning1",
+			cant = { [1] = "cantdo4" },
+			count = { [1] = "count6", [2] = "count5", [3] = "count4", [4] = "count3", [5] = "count2", [6] = "count1" },
+			ok = { [1] = "drone1" },
+			select = { [1] = "drone1" },
+		},
+		weapondefs = {
+			repulsor = {
+				avoidfeature = false,
+				craterareaofeffect = 0,
+				craterboost = 0,
+				cratermult = 0,
+				edgeeffectiveness = 0.15,
+				name = "PlasmaRepulsor",
+				range = 1,
+				weapontype = "Shield",
+				damage = { default = 1 },
+				shield = {
+					alpha = 0.17,
+					armortype = "shields",
+					exterior = true,
+					force = 2.5,
+					intercepttype = 951,
+					power = 200,
+					powerregen = 10,
+					powerregenenergy = 200,
+					radius = 100,
+					startingpower = 0,
+					visiblerepulse = true,
+				},
+			},
+		},
+		weapons = {
+			[1] = {
+				def = "REPULSOR",
+				onlytargetcategory = "NOTSUB",
+			},
+		},
+	},
+}

units/Legion/TechCore/legkeystone.lua
@@ -1,0 +1,133 @@
+return {
+	legkeystone = {
+		activatewhenbuilt = true,
+		buildangle = 4096,
+		builddistance = 150,
+		builder = true,
+		buildoptions = {
+			[1] = "legvoussoir",
+		},
+		buildpic = "LEGGATET3.DDS",
+		buildtime = 15000,
+		workertime = 200,
+		onoffable = true,
+		canattack = false,
+		canrepeat = false,
+		category = "NOWEAPON",
+		collisionvolumeoffsets = "0 0 0",
+		collisionvolumescales = "88 70 88",
+		collisionvolumetype = "CylY",
+		damagemodifier = 1,
+		energycost = 10000,
+		energyupkeep = -100,
+		energystorage = 1000,
+		explodeas = "fusionExplosion",
+		footprintx = 6,
+		footprintz = 6,
+		health = 2000,
+		idleautoheal = 5,
+		idletime = 1800,
+		maxacc = 0,
+		maxdec = 0,
+		maxslope = 10,
+		maxwaterdepth = 1000,
+		metalcost = 1000,
+		noautofire = true,
+		objectname = "Units/LEGGATET3.s3o",
+		script = "Units/legkeystone.lua",
+		seismicsignature = 0,
+		selfdestructas = "fusionExplosionSelfd",
+		sightdistance = 350,
+		customparams = {
+			buildinggrounddecaldecayspeed = 30,
+			buildinggrounddecalsizex = 8,
+			buildinggrounddecalsizey = 8,
+			buildinggrounddecaltype = "decals/leggate_aoplane.dds",
+			model_author = "Beherith/Protar",
+			normaltex = "unittextures/Leg_normal.dds",
+			removestop = true,
+			removewait = true,
+			shield_color_mult = 25,
+			shield_power = 200,
+			shield_radius = 150,
+			subfolder = "Legion/TechCore",
+			tech_core_value = 1,
+			techlevel = 1,
+			unitgroup = "energy",
+			usebuildinggrounddecal = true,
+		},
+		featuredefs = {
+			dead = {
+				blocking = true,
+				category = "corpses",
+				collisionvolumeoffsets = "0 0 0",
+				collisionvolumescales = "88 55 88",
+				collisionvolumetype = "CylY",
+				damage = 8500,
+				footprintx = 6,
+				footprintz = 6,
+				height = 20,
+				metal = 100,
+				object = "Units/leggatet3_dead.s3o",
+				resurrectable = 0,
+			},
+			heap = {
+				blocking = false,
+				category = "heaps",
+				damage = 4250,
+				footprintx = 5,
+				footprintz = 5,
+				height = 4,
+				metal = 150,
+				resurrectable = 0,
+			},
+		},
+		sfxtypes = {
+			pieceexplosiongenerators = {
+				[1] = "deathceg2",
+				[2] = "deathceg3",
+				[3] = "deathceg4",
+			},
+		},
+		sounds = {
+			canceldestruct = "cancel2",
+			underattack = "warning1",
+			cant = { [1] = "cantdo4" },
+			count = { [1] = "count6", [2] = "count5", [3] = "count4", [4] = "count3", [5] = "count2", [6] = "count1" },
+			ok = { [1] = "drone1" },
+			select = { [1] = "drone1" },
+		},
+		weapondefs = {
+			repulsor = {
+				avoidfeature = false,
+				craterareaofeffect = 0,
+				craterboost = 0,
+				cratermult = 0,
+				edgeeffectiveness = 0.15,
+				name = "PlasmaRepulsor",
+				range = 1,
+				weapontype = "Shield",
+				damage = { default = 1 },
+				shield = {
+					alpha = 0.17,
+					armortype = "shields",
+					exterior = true,
+					force = 2.5,
+					intercepttype = 951,
+					power = 200,
+					powerregen = 10,
+					powerregenenergy = 200,
+					radius = 100,
+					startingpower = 0,
+					visiblerepulse = true,
+				},
+			},
+		},
+		weapons = {
+			[1] = {
+				def = "REPULSOR",
+				onlytargetcategory = "NOTSUB",
+			},
+		},
+	},
+}

keithharvey/bar

units/ArmBuildings/TechCore/armkeystone.lua
@@ -1,0 +1,137 @@
+return {
+	armkeystone = {
+		activatewhenbuilt = true,
+		buildangle = 2048,
+		builddistance = 150,
+		builder = true,
+		buildoptions = {
+			[1] = "armvoussoir",
+		},
+		buildpic = "ARMGATET3.DDS",
+		buildtime = 15000,
+		workertime = 200,
+		onoffable = true,
+		canattack = false,
+		canrepeat = false,
+		category = "NOWEAPON",
+		collisionvolumeoffsets = "0 0 3",
+		collisionvolumescales = "91 85 91",
+		collisionvolumetype = "CylY",
+		energycost = 10000,
+		energymake = 100,
+		energyupkeep = 100,
+		energystorage = 1000,
+		explodeas = "fusionExplosion",
+		floater = true,
+		footprintx = 6,
+		footprintz = 6,
+		health = 2000,
+		idleautoheal = 5,
+		idletime = 1800,
+		maxacc = 0,
+		maxdec = 0,
+		maxslope = 10,
+		maxwaterdepth = 50000000,
+		metalcost = 1000,
+		noautofire = true,
+		objectname = "Units/ARMGATET3.s3o",
+		script = "Units/armkeystone.lua",
+		seismicsignature = 0,
+		selfdestructas = "fusionExplosionSelfd",
+		sightdistance = 350,
+		waterline = 6,
+		customparams = {
+			buildinggrounddecaldecayspeed = 30,
+			buildinggrounddecalsizex = 9,
+			buildinggrounddecalsizey = 9,
+			buildinggrounddecaltype = "decals/armgate_aoplane.dds",
+			model_author = "Beherith/Protar",
+			normaltex = "unittextures/Arm_normal.dds",
+			removestop = true,
+			removewait = true,
+			shield_color_mult = 25,
+			shield_power = 200,
+			shield_radius = 150,
+			shield_downtime_mult = 0.1,
+			subfolder = "ArmBuildings/TechCore",
+			tech_core_value = 1,
+			techlevel = 1,
+			unitgroup = "builder",
+			usebuildinggrounddecal = true,
+		},
+		featuredefs = {
+			dead = {
+				blocking = true,
+				category = "corpses",
+				collisionvolumeoffsets = "0 0 3",
+				collisionvolumescales = "91 67 91",
+				collisionvolumetype = "CylY",
+				damage = 8500,
+				footprintx = 6,
+				footprintz = 6,
+				height = 20,
+				metal = 100,
+				object = "Units/armgatet3_dead.s3o",
+				resurrectable = 0,
+			},
+			heap = {
+				blocking = false,
+				category = "heaps",
+				damage = 4250,
+				footprintx = 5,
+				footprintz = 5,
+				height = 4,
+				metal = 150,
+				resurrectable = 0,
+			},
+		},
+		sfxtypes = {
+			pieceexplosiongenerators = {
+				[1] = "deathceg2",
+				[2] = "deathceg3",
+				[3] = "deathceg4",
+			},
+		},
+		sounds = {
+			canceldestruct = "cancel2",
+			underattack = "warning1",
+			cant = { [1] = "cantdo4" },
+			count = { [1] = "count6", [2] = "count5", [3] = "count4", [4] = "count3", [5] = "count2", [6] = "count1" },
+			ok = { [1] = "drone1" },
+			select = { [1] = "drone1" },
+		},
+		weapondefs = {
+			repulsor = {
+				avoidfeature = false,
+				craterareaofeffect = 0,
+				craterboost = 0,
+				cratermult = 0,
+				edgeeffectiveness = 0.15,
+				name = "PlasmaRepulsor",
+				range = 1,
+				weapontype = "Shield",
+				damage = { default = 1 },
+				shield = {
+					alpha = 0.17,
+					armortype = "shields",
+					exterior = true,
+					energyupkeep = 0,
+					force = 2.5,
+					intercepttype = 1,
+					power = 200,
+					powerregen = 10,
+					powerregenenergy = 0,
+					radius = 100,
+					startingpower = 200,
+					visiblerepulse = true,
+				},
+			},
+		},
+		weapons = {
+			[1] = {
+				def = "REPULSOR",
+				onlytargetcategory = "NOTSUB",
+			},
+		},
+	},
+}

units/CorBuildings/TechCore/corkeystone.lua
@@ -1,0 +1,137 @@
+return {
+	corkeystone = {
+		activatewhenbuilt = true,
+		buildangle = 2048,
+		builddistance = 150,
+		builder = true,
+		buildoptions = {
+			[1] = "corvoussoir",
+		},
+		buildpic = "CORGATET3.DDS",
+		buildtime = 15000,
+		workertime = 200,
+		onoffable = true,
+		canattack = false,
+		canrepeat = false,
+		category = "NOWEAPON",
+		collisionvolumeoffsets = "0 0 3",
+		collisionvolumescales = "91 85 91",
+		collisionvolumetype = "CylY",
+		energycost = 10000,
+		energymake = 100,
+		energyupkeep = 100,
+		energystorage = 1000,
+		explodeas = "fusionExplosion",
+		floater = true,
+		footprintx = 6,
+		footprintz = 6,
+		health = 2000,
+		idleautoheal = 5,
+		idletime = 1800,
+		maxacc = 0,
+		maxdec = 0,
+		maxslope = 10,
+		maxwaterdepth = 50000000,
+		metalcost = 1000,
+		noautofire = true,
+		objectname = "Units/CORGATET3.s3o",
+		script = "Units/corkeystone.lua",
+		seismicsignature = 0,
+		selfdestructas = "fusionExplosionSelfd",
+		sightdistance = 350,
+		waterline = 6,
+		customparams = {
+			buildinggrounddecaldecayspeed = 30,
+			buildinggrounddecalsizex = 9,
+			buildinggrounddecalsizey = 9,
+			buildinggrounddecaltype = "decals/corgate_aoplane.dds",
+			model_author = "Beherith/Protar",
+			normaltex = "unittextures/Cor_normal.dds",
+			removestop = true,
+			removewait = true,
+			shield_color_mult = 25,
+			shield_power = 200,
+			shield_radius = 150,
+			shield_downtime_mult = 0.1,
+			subfolder = "CorBuildings/TechCore",
+			tech_core_value = 1,
+			techlevel = 1,
+			unitgroup = "builder",
+			usebuildinggrounddecal = true,
+		},
+		featuredefs = {
+			dead = {
+				blocking = true,
+				category = "corpses",
+				collisionvolumeoffsets = "0 0 3",
+				collisionvolumescales = "91 67 91",
+				collisionvolumetype = "CylY",
+				damage = 8500,
+				footprintx = 6,
+				footprintz = 6,
+				height = 20,
+				metal = 100,
+				object = "Units/corgatet3_dead.s3o",
+				resurrectable = 0,
+			},
+			heap = {
+				blocking = false,
+				category = "heaps",
+				damage = 4250,
+				footprintx = 5,
+				footprintz = 5,
+				height = 4,
+				metal = 150,
+				resurrectable = 0,
+			},
+		},
+		sfxtypes = {
+			pieceexplosiongenerators = {
+				[1] = "deathceg2",
+				[2] = "deathceg3",
+				[3] = "deathceg4",
+			},
+		},
+		sounds = {
+			canceldestruct = "cancel2",
+			underattack = "warning1",
+			cant = { [1] = "cantdo4" },
+			count = { [1] = "count6", [2] = "count5", [3] = "count4", [4] = "count3", [5] = "count2", [6] = "count1" },
+			ok = { [1] = "drone1" },
+			select = { [1] = "drone1" },
+		},
+		weapondefs = {
+			repulsor = {
+				avoidfeature = false,
+				craterareaofeffect = 0,
+				craterboost = 0,
+				cratermult = 0,
+				edgeeffectiveness = 0.15,
+				name = "PlasmaRepulsor",
+				range = 1,
+				weapontype = "Shield",
+				damage = { default = 1 },
+				shield = {
+					alpha = 0.17,
+					armortype = "shields",
+					exterior = true,
+					energyupkeep = 0,
+					force = 2.5,
+					intercepttype = 1,
+					power = 200,
+					powerregen = 10,
+					powerregenenergy = 0,
+					radius = 100,
+					startingpower = 200,
+					visiblerepulse = true,
+				},
+			},
+		},
+		weapons = {
+			[1] = {
+				def = "REPULSOR",
+				onlytargetcategory = "NOTSUB",
+			},
+		},
+	},
+}

units/Legion/TechCore/legkeystone.lua
@@ -1,0 +1,138 @@
+return {
+	legkeystone = {
+		activatewhenbuilt = true,
+		buildangle = 4096,
+		builddistance = 150,
+		builder = true,
+		buildoptions = {
+			[1] = "legvoussoir",
+		},
+		buildpic = "LEGGATET3.DDS",
+		buildtime = 15000,
+		workertime = 200,
+		onoffable = true,
+		canattack = false,
+		canrepeat = false,
+		category = "NOWEAPON",
+		collisionvolumeoffsets = "0 0 0",
+		collisionvolumescales = "88 70 88",
+		collisionvolumetype = "CylY",
+		damagemodifier = 1,
+		energycost = 10000,
+		energymake = 100,
+		energyupkeep = 100,
+		energystorage = 1000,
+		explodeas = "fusionExplosion",
+		floater = true,
+		footprintx = 6,
+		footprintz = 6,
+		health = 2000,
+		idleautoheal = 5,
+		idletime = 1800,
+		maxacc = 0,
+		maxdec = 0,
+		maxslope = 10,
+		maxwaterdepth = 50000000,
+		metalcost = 1000,
+		noautofire = true,
+		objectname = "Units/LEGGATET3.s3o",
+		script = "Units/legkeystone.lua",
+		seismicsignature = 0,
+		selfdestructas = "fusionExplosionSelfd",
+		sightdistance = 350,
+		waterline = 6,
+		customparams = {
+			buildinggrounddecaldecayspeed = 30,
+			buildinggrounddecalsizex = 8,
+			buildinggrounddecalsizey = 8,
+			buildinggrounddecaltype = "decals/leggate_aoplane.dds",
+			model_author = "Beherith/Protar",
+			normaltex = "unittextures/Leg_normal.dds",
+			removestop = true,
+			removewait = true,
+			shield_color_mult = 25,
+			shield_power = 200,
+			shield_radius = 150,
+			shield_downtime_mult = 0.1,
+			subfolder = "Legion/TechCore",
+			tech_core_value = 1,
+			techlevel = 1,
+			unitgroup = "builder",
+			usebuildinggrounddecal = true,
+		},
+		featuredefs = {
+			dead = {
+				blocking = true,
+				category = "corpses",
+				collisionvolumeoffsets = "0 0 0",
+				collisionvolumescales = "88 55 88",
+				collisionvolumetype = "CylY",
+				damage = 8500,
+				footprintx = 6,
+				footprintz = 6,
+				height = 20,
+				metal = 100,
+				object = "Units/leggatet3_dead.s3o",
+				resurrectable = 0,
+			},
+			heap = {
+				blocking = false,
+				category = "heaps",
+				damage = 4250,
+				footprintx = 5,
+				footprintz = 5,
+				height = 4,
+				metal = 150,
+				resurrectable = 0,
+			},
+		},
+		sfxtypes = {
+			pieceexplosiongenerators = {
+				[1] = "deathceg2",
+				[2] = "deathceg3",
+				[3] = "deathceg4",
+			},
+		},
+		sounds = {
+			canceldestruct = "cancel2",
+			underattack = "warning1",
+			cant = { [1] = "cantdo4" },
+			count = { [1] = "count6", [2] = "count5", [3] = "count4", [4] = "count3", [5] = "count2", [6] = "count1" },
+			ok = { [1] = "drone1" },
+			select = { [1] = "drone1" },
+		},
+		weapondefs = {
+			repulsor = {
+				avoidfeature = false,
+				craterareaofeffect = 0,
+				craterboost = 0,
+				cratermult = 0,
+				edgeeffectiveness = 0.15,
+				name = "PlasmaRepulsor",
+				range = 1,
+				weapontype = "Shield",
+				damage = { default = 1 },
+				shield = {
+					alpha = 0.17,
+					armortype = "shields",
+					exterior = true,
+					energyupkeep = 0,
+					force = 2.5,
+					intercepttype = 1,
+					power = 200,
+					powerregen = 10,
+					powerregenenergy = 0,
+					radius = 100,
+					startingpower = 200,
+					visiblerepulse = true,
+				},
+			},
+		},
+		weapons = {
+			[1] = {
+				def = "REPULSOR",
+				onlytargetcategory = "NOTSUB",
+			},
+		},
+	},
+}