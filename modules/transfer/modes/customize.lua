beyond-all-reason/Beyond-All-Reason

transfer: what may pass between allied teams
Units, resources and an empty team's assets all move by the same
question — may this pass, at what cost, and who is told. One pipeline
answers it, so there is a single place where a transfer is validated,
priced and announced rather than one per thing being moved.

Sharing was never a domain. It was a feature name over this pipeline
plus four construction gadgets and a tech feature, which is why every
boundary drawn around it failed: allied is a modifier on other domains'
policies, not a domain of its own. What is left once those leave is one
system, and transfer is what it does.

The tax rate comes with it. What a transfer costs is a property of the
transfer; it reads the tier tech owns, and that edge runs one way.

Shared spec builders gain the fixtures these specs need. They are only
used here, so they travel with the module that needs them.

modules/transfer/modes/customize.lua
@@ -1,0 +1,24 @@
+local ModeDSL = VFS.Include("modules/transfer/mode_dsl.lua")
+local Mode, Share, Assist, Reclaim, Resurrect, Build, Take, Tech =
+	ModeDSL.Mode, ModeDSL.Share, ModeDSL.Assist, ModeDSL.Reclaim, ModeDSL.Resurrect, ModeDSL.Build, ModeDSL.Take, ModeDSL.Tech
+
+-- Every policy exposed unlocked; values are only the starting point (RetainValues).
+return Mode("Customize")
+	.Desc("Tweak everything. Switching to Customize keeps the current mode's settings as a starting point (defaults if you start here).")
+	.Ranked()
+	.RetainValues()
+	.Open(Tech, 1, 1.5).Unlocked()
+	.Allow(Share.Units).Unlocked()
+	.Deny(Share.Units.AtT2).Unlocked()
+	.Deny(Share.Units.AtT3).Unlocked()
+	.Stun(Share.Units.Resource, 0).Unlocked()
+	.Delay(Build.Constructors, 0).Unlocked()
+	.Allow(Share.Resources).Unlocked()
+	.Tax(Share.Resources, 0).Unlocked()
+	.Tax(Share.Resources.AtT2, -1).Unlocked()
+	.Tax(Share.Resources.AtT3, -1).Unlocked()
+	.Allow(Assist.Allied).Unlocked()
+	.Allow(Reclaim.AlliedUnits).Unlocked()
+	.Allow(Resurrect.Partial).Unlocked()
+	.Allow(Take).Unlocked()
+	.Delay(Take.Resource, 30).Unlocked()

modules/transfer/modes/disabled.lua
@@ -1,0 +1,12 @@
+local ModeDSL = VFS.Include("modules/transfer/mode_dsl.lua")
+local Mode, Share, Assist, Reclaim, Take = ModeDSL.Mode, ModeDSL.Share, ModeDSL.Assist, ModeDSL.Reclaim, ModeDSL.Take
+
+return Mode("Disabled")
+	.Desc("Disable all sharing; apply a 30% tax; lock most controls.")
+	.Ranked()
+	.Deny(Share.Units)
+	.Deny(Share.Resources)
+	.Tax(Share.Resources, 0.30).Hidden().Unlocked()
+	.Deny(Assist.Allied)
+	.Deny(Reclaim.AlliedUnits)
+	.Deny(Take).Unlocked()

modules/transfer/modes/easy_tax.lua
@@ -1,0 +1,17 @@
+local ModeDSL = VFS.Include("modules/transfer/mode_dsl.lua")
+local Mode, Share, Assist, Reclaim, Resurrect, Build, Take =
+	ModeDSL.Mode, ModeDSL.Share, ModeDSL.Assist, ModeDSL.Reclaim, ModeDSL.Resurrect, ModeDSL.Build, ModeDSL.Take
+
+return Mode("Easy Tax")
+	.Desc("Anti co-op sharing tax mode. Tax on resource sharing, assist, and resurrection. Eco buildings stunned, mobile constructors debuffed.")
+	.Ranked()
+	.Allow(Share.Units)
+	.Stun(Share.Units.Resource, 30)
+	.Delay(Build.Constructors, 30)
+	.Allow(Share.Resources)
+	.Tax(Share.Resources, 0.30)
+	.Allow(Assist.Allied)
+	.Allow(Reclaim.AlliedUnits)
+	.Allow(Resurrect.Partial)
+	.Stun(Take)
+	.Delay(Take.Resource, 30)

modules/transfer/modes/enabled.lua
@@ -1,0 +1,12 @@
+local ModeDSL = VFS.Include("modules/transfer/mode_dsl.lua")
+local Mode, Share, Assist, Reclaim, Take = ModeDSL.Mode, ModeDSL.Share, ModeDSL.Assist, ModeDSL.Reclaim, ModeDSL.Take
+
+return Mode("Enabled")
+	.Desc("All sharing on with fixed defaults.")
+	.Ranked()
+	.Allow(Share.Units)
+	.Allow(Share.Resources)
+	.Tax(Share.Resources, 0.0).Hidden().Locked()
+	.Allow(Assist.Allied)
+	.Allow(Reclaim.AlliedUnits)
+	.Allow(Take)

modules/transfer/modes/tech_core.lua
@@ -1,0 +1,20 @@
+local ModeDSL = VFS.Include("modules/transfer/mode_dsl.lua")
+local Mode, Share, Assist, Reclaim, Resurrect, Take, Tech =
+	ModeDSL.Mode, ModeDSL.Share, ModeDSL.Assist, ModeDSL.Reclaim, ModeDSL.Resurrect, ModeDSL.Take, ModeDSL.Tech
+
+return Mode("Tech Core")
+	.Desc("Tech levels gate unit construction. Build Keystone buildings to advance. Sharing unlocks with tech. Legion's mex economy is rebalanced for the universal Voussoir.")
+	.Ranked()
+	.Gate(Tech, 1, 1.5)
+	.Deny(Share.Units)
+	.Allow(Share.Units.Constructors.AtT2)
+	.Deny(Share.Units.AtT3)
+	.Allow(Share.Resources)
+	.Tax(Share.Resources, 0.6)
+	.Tax(Share.Resources.AtT2, 0.5)
+	.Tax(Share.Resources.AtT3, 0.4)
+	.Allow(Assist.Allied)
+	.Allow(Reclaim.AlliedUnits)
+	.Deny(Resurrect.Partial)
+	.Defer(Take)
+	.Delay(Take.Resource, 60)

keithharvey/bar

modules/transfer/modes/customize.lua
@@ -1,0 +1,24 @@
+local ModeDSL = VFS.Include("modules/transfer/mode_dsl.lua")
+local Mode, Transfer, Assist, Reclaim, Resurrect, Build, Take, Tech =
+	ModeDSL.Mode, ModeDSL.Transfer, ModeDSL.Assist, ModeDSL.Reclaim, ModeDSL.Resurrect, ModeDSL.Build, ModeDSL.Take, ModeDSL.Tech
+
+-- Every policy exposed unlocked; values are only the starting point (RetainValues).
+return Mode("Customize")
+	.Desc("Tweak everything. Switching to Customize keeps the current mode's settings as a starting point (defaults if you start here).")
+	.Ranked()
+	.RetainValues()
+	.Open(Tech, 1, 1.5).Unlocked()
+	.Allow(Transfer.Units).Unlocked()
+	.Deny(Transfer.Units.AtT2).Unlocked()
+	.Deny(Transfer.Units.AtT3).Unlocked()
+	.Stun(Transfer.Units.Resource, 0).Unlocked()
+	.Delay(Build.Constructors, 0).Unlocked()
+	.Allow(Transfer.Resources).Unlocked()
+	.Tax(Transfer.Resources, 0).Unlocked()
+	.Tax(Transfer.Resources.AtT2, -1).Unlocked()
+	.Tax(Transfer.Resources.AtT3, -1).Unlocked()
+	.Allow(Assist.Allied).Unlocked()
+	.Allow(Reclaim.AlliedUnits).Unlocked()
+	.Allow(Resurrect.Partial).Unlocked()
+	.Allow(Take).Unlocked()
+	.Delay(Take.Resource, 30).Unlocked()

modules/transfer/modes/disabled.lua
@@ -1,0 +1,12 @@
+local ModeDSL = VFS.Include("modules/transfer/mode_dsl.lua")
+local Mode, Transfer, Assist, Reclaim, Take = ModeDSL.Mode, ModeDSL.Transfer, ModeDSL.Assist, ModeDSL.Reclaim, ModeDSL.Take
+
+return Mode("Disabled")
+	.Desc("Disable all sharing; apply a 30% tax; lock most controls.")
+	.Ranked()
+	.Deny(Transfer.Units)
+	.Deny(Transfer.Resources)
+	.Tax(Transfer.Resources, 0.30).Hidden().Unlocked()
+	.Deny(Assist.Allied)
+	.Deny(Reclaim.AlliedUnits)
+	.Deny(Take).Unlocked()

modules/transfer/modes/easy_tax.lua
@@ -1,0 +1,17 @@
+local ModeDSL = VFS.Include("modules/transfer/mode_dsl.lua")
+local Mode, Transfer, Assist, Reclaim, Resurrect, Build, Take =
+	ModeDSL.Mode, ModeDSL.Transfer, ModeDSL.Assist, ModeDSL.Reclaim, ModeDSL.Resurrect, ModeDSL.Build, ModeDSL.Take
+
+return Mode("Easy Tax")
+	.Desc("Anti co-op sharing tax mode. Tax on resource sharing, assist, and resurrection. Eco buildings stunned, mobile constructors debuffed.")
+	.Ranked()
+	.Allow(Transfer.Units)
+	.Stun(Transfer.Units.Resource, 30)
+	.Delay(Build.Constructors, 30)
+	.Allow(Transfer.Resources)
+	.Tax(Transfer.Resources, 0.30)
+	.Allow(Assist.Allied)
+	.Allow(Reclaim.AlliedUnits)
+	.Allow(Resurrect.Partial)
+	.Stun(Take)
+	.Delay(Take.Resource, 30)

modules/transfer/modes/enabled.lua
@@ -1,0 +1,12 @@
+local ModeDSL = VFS.Include("modules/transfer/mode_dsl.lua")
+local Mode, Transfer, Assist, Reclaim, Take = ModeDSL.Mode, ModeDSL.Transfer, ModeDSL.Assist, ModeDSL.Reclaim, ModeDSL.Take
+
+return Mode("Enabled")
+	.Desc("All sharing on with fixed defaults.")
+	.Ranked()
+	.Allow(Transfer.Units)
+	.Allow(Transfer.Resources)
+	.Tax(Transfer.Resources, 0.0).Hidden().Locked()
+	.Allow(Assist.Allied)
+	.Allow(Reclaim.AlliedUnits)
+	.Allow(Take)

modules/transfer/modes/tech_core.lua
@@ -1,0 +1,20 @@
+local ModeDSL = VFS.Include("modules/transfer/mode_dsl.lua")
+local Mode, Transfer, Assist, Reclaim, Resurrect, Take, Tech =
+	ModeDSL.Mode, ModeDSL.Transfer, ModeDSL.Assist, ModeDSL.Reclaim, ModeDSL.Resurrect, ModeDSL.Take, ModeDSL.Tech
+
+return Mode("Tech Core")
+	.Desc("Tech levels gate unit construction. Build Keystone buildings to advance. Sharing unlocks with tech. Legion's mex economy is rebalanced for the universal Voussoir.")
+	.Ranked()
+	.Gate(Tech, 1, 1.5)
+	.Deny(Transfer.Units)
+	.Allow(Transfer.Units.Constructors.AtT2)
+	.Deny(Transfer.Units.AtT3)
+	.Allow(Transfer.Resources)
+	.Tax(Transfer.Resources, 0.6)
+	.Tax(Transfer.Resources.AtT2, 0.5)
+	.Tax(Transfer.Resources.AtT3, 0.4)
+	.Allow(Assist.Allied)
+	.Allow(Reclaim.AlliedUnits)
+	.Deny(Resurrect.Partial)
+	.Defer(Take)
+	.Delay(Take.Resource, 60)