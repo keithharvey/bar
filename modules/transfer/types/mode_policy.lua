e982a8c6016cfdff2e4cbd04853157d295a84947
Daniel Harvey <keithdanielharvey@gmail.com>
2026-09-15 15:33:29 -0600

    fixup! transfer: what may pass between allied teams

--- modules/transfer/types/mode_policy.lua
+++ modules/transfer/types/mode_policy.lua
@@ -17,7 +17,7 @@
 ---@field Delay fun(noun: TransferGrant, seconds: number): TransferModeChain
 ---@field Gate fun(noun: TransferGrant, t2: number, t3: number): TransferModeChain
 ---@field Open fun(noun: TransferGrant, t2: number, t3: number): TransferModeChain
----@field MexSplitting fun(which: MexSplittingKey): TransferModeChain Where a team's mex income comes from: writes mex_splitting with the given key. A rule verb, so it comes back pinned.
+---@field MexSplitting fun(which: MexSplittingKey): TransferModeChain How a lobby chooses to allocate a team's mex income
 
 ---@param name string
 ---@return TransferModeChain
